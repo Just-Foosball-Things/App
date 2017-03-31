25ee5737cb7e143462c43b7233c0a70013f68525
Oscar de Leeuw <o.deleeuw@student.fontys.nl>
2017-03-31 09:57:39 +0200

    Tidied up listening for click events.

--- app/src/main/java/nl/jft/overview/fragment/MatchesFragment.java
+++ app/src/main/java/nl/jft/overview/fragment/MatchesFragment.java
@@ -1,14 +1,14 @@
 package nl.jft.overview.fragment;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ExpandableListView;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Random;
 
 import nl.jft.CustomFragment;
@@ -20,7 +20,6 @@ import nl.jft.logic.match.MatchType;
 import nl.jft.logic.participant.Participant;
 import nl.jft.logic.participant.Title;
 import nl.jft.logic.participant.impl.User;
-import nl.jft.match.MatchActivity;
 import nl.jft.widget.AnimatedExpandableListView;
 import nl.jft.widget.match.MatchesOverviewListAdapter;
 
@@ -31,8 +30,47 @@ import nl.jft.widget.match.MatchesOverviewListAdapter;
 public class MatchesFragment extends CustomFragment {
 
     private static Random random = new Random();
+    private List<Match> matches;
 
-    private static Match createRandomMatch(Participant firstParticipant, Participant secondParticipant) {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_matches, container, false);
+        AnimatedExpandableListView listView = (AnimatedExpandableListView) rootView.findViewById(R.id.overview_matches_listview);
+
+        matches = getMatches();
+        final MatchesOverviewListAdapter adapter = new MatchesOverviewListAdapter(inflater.getContext());
+        adapter.addMatches(matches);
+
+        listView.setAdapter(adapter);
+        MatchesFragmentClickListener listener = new MatchesFragmentClickListener(matches);
+
+        listView.setOnGroupClickListener(listener);
+        listView.setOnChildClickListener(listener);
+
+        return rootView;
+    }
+
+    @Override
+    public CharSequence getTitle() {
+        return "Matches";
+    }
+
+    private List<Match> getMatches() {
+        List<Match> matches = new ArrayList<>();
+
+        final Participant lesley = new User("Lesley", new GlickoRating(1500, 350, 0.06), new Title("Weltmeister"));
+        final Participant oscar = new User("Oscar", new GlickoRating(1500, 350, 0.06), new Title("Weltmeister"));
+
+        for (int i = 0; i < 10; i++) {
+            matches.add(createRandomMatch(lesley, oscar));
+            matches.add(createRandomMatch(oscar, lesley));
+        }
+
+        return matches;
+    }
+
+    private Match createRandomMatch(Participant firstParticipant, Participant secondParticipant) {
         Match match = new Match(firstParticipant, secondParticipant, MatchType.FRIENDLY);
 
         int goalsFirstParticipant = 0;
@@ -54,49 +92,5 @@ public class MatchesFragment extends CustomFragment {
         }
 
         return match;
-    }
-
-    @Nullable
-    @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_matches, container, false);
-        AnimatedExpandableListView listView = (AnimatedExpandableListView) rootView.findViewById(R.id.overview_matches_listview);
-
-        final MatchesOverviewListAdapter adapter = new MatchesOverviewListAdapter(inflater.getContext());
-
-        final Participant lesley = new User("Lesley", new GlickoRating(1500, 350, 0.06), new Title("Weltmeister"));
-        final Participant oscar = new User("Oscar", new GlickoRating(1500, 350, 0.06), new Title("Weltmeister"));
-
-        Match match1 = createRandomMatch(lesley, oscar);
-        Match match2 = createRandomMatch(oscar, lesley);
-
-        adapter.addMatch(match1);
-        adapter.addMatch(match2);
-
-        listView.setAdapter(adapter);
-        listView.setOnGroupClickListener(new ExpandableListView.OnGroupClickListener() {
-            @Override
-            public boolean onGroupClick(ExpandableListView parent, View v, int groupPosition, long id) {
-                Intent intent = new Intent(v.getContext(), MatchActivity.class);
-                startActivity(intent);
-                return true;
-            }
-        });
-
-        listView.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
-            @Override
-            public boolean onChildClick(ExpandableListView parent, View v, int groupPosition, int childPosition, long id) {
-                Intent intent = new Intent(v.getContext(), MatchActivity.class);
-                startActivity(intent);
-                return true;
-            }
-        });
-
-        return rootView;
-    }
-
-    @Override
-    public CharSequence getTitle() {
-        return "Matches";
     }
 }
